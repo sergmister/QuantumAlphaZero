a42766afbea7c99566223afc6de025d0cb99a762
Sergii Penner <sergmister1@gmail.com>
2024-04-16 23:20:25 -0600

    Custom board size

---
+++ othello_game.py
@@ -0,0 +1,216 @@
+import numpy as np
+from open_spiel.python.observation import IIGObserverForPublicInfoGame
+import pyspiel
+
+DIRS = ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1))
+DEFAULT_N = 4
+
+num_players = 2
+game_type = pyspiel.GameType(
+    short_name="othello_nxn",
+    long_name="Python Othello nxn",
+    dynamics=pyspiel.GameType.Dynamics.SEQUENTIAL,
+    chance_mode=pyspiel.GameType.ChanceMode.DETERMINISTIC,
+    information=pyspiel.GameType.Information.PERFECT_INFORMATION,
+    utility=pyspiel.GameType.Utility.ZERO_SUM,
+    reward_model=pyspiel.GameType.RewardModel.TERMINAL,
+    max_num_players=num_players,
+    min_num_players=num_players,
+    provides_information_state_string=True,
+    provides_information_state_tensor=False,
+    provides_observation_string=True,
+    provides_observation_tensor=True,
+    parameter_specification={},
+)
+
+
+class OthelloGame(pyspiel.Game):
+    def __init__(self, params=dict()):
+        self.n = params.get("n", DEFAULT_N)
+        assert self.n % 2 == 0
+        num_cells = self.n * self.n
+        game_info = pyspiel.GameInfo(
+            num_distinct_actions=num_cells,
+            max_chance_outcomes=0,
+            num_players=2,
+            min_utility=-1.0,
+            max_utility=1.0,
+            utility_sum=0.0,
+            max_game_length=num_cells,
+        )
+        super().__init__(game_type, game_info, params)
+
+    def new_initial_state(self):
+        """Returns a state corresponding to the start of a game."""
+        return OthelloState(self)
+
+    def make_py_observer(self, iig_obs_type=None, params=None):
+        """Returns an object used for observing game state."""
+        if (iig_obs_type is None) or (iig_obs_type.public_info and not iig_obs_type.perfect_recall):
+            return BoardObserver(params)
+        else:
+            return IIGObserverForPublicInfoGame(iig_obs_type, params)
+
+
+class OthelloState(pyspiel.State):
+    def __init__(self, game):
+        """Constructor; should only be called by Game.new_initial_state."""
+        super().__init__(game)
+        self.n = game.n
+        self._cur_player = 0
+        self._player0_score = 0.0
+        self._is_terminal = False
+        # 1 for black, -1 for white, 0 for empty
+        self.board = np.zeros((self.n, self.n), np.int8)
+        self.board[self.n // 2 - 1][self.n // 2 - 1] = -1
+        self.board[self.n // 2][self.n // 2] = -1
+        self.board[self.n // 2 - 1][self.n // 2] = 1
+        self.board[self.n // 2][self.n // 2 - 1] = 1
+
+    # OpenSpiel (PySpiel) API functions are below. This is the standard set that
+    # should be implemented by every perfect-information sequential-move game.
+
+    def current_player(self):
+        """Returns id of the next player to move, or TERMINAL if game is over."""
+        return pyspiel.PlayerId.TERMINAL if self._is_terminal else self._cur_player
+
+    def _get_turn(self, player=None):
+        if player is not None:
+            return 1 if player == 0 else -1
+        else:
+            return 1 if self._cur_player == 0 else -1
+
+    def _get_action(self, x, y):
+        return x * self.n + y
+
+    def _get_coord(self, action):
+        return (action // self.n, action % self.n)
+
+    def _in_board(self, x, y):
+        return 0 <= x < self.n and 0 <= y < self.n
+
+    def _legal_action(self, action, player=None):
+        turn = self._get_turn(player)
+        x, y = self._get_coord(action)
+        if self.board[x][y] != 0:
+            return False
+        for xdir, ydir in DIRS:
+            px = x
+            py = y
+            onum = False
+            while True:
+                px += xdir
+                py += ydir
+                if self._in_board(px, py):
+                    if self.board[px][py] == -turn:
+                        onum = True
+                    elif self.board[px][py] == turn:
+                        if onum:
+                            return True
+                        else:
+                            break
+                    else:
+                        break
+                else:
+                    break
+
+    def _legal_actions(self, player):
+        """Returns a list of legal actions, sorted in ascending order."""
+        return [a for a in range(self.n * self.n) if self._legal_action(a, player)]
+
+    def _apply_action(self, action):
+        """Applies the specified action to the state."""
+        turn = self._get_turn()
+        x, y = self._get_coord(action)
+        self.board[x][y] = turn
+        for xdir, ydir in DIRS:
+            cpos = []
+            px = x
+            py = y
+            found = False
+            while True:
+                px += xdir
+                py += ydir
+                if self._in_board(px, py):
+                    if self.board[px][py] == 0:
+                        break
+                    elif self.board[px][py] == -turn:
+                        cpos.append((px, py))
+                    elif self.board[px][py] == turn:
+                        if len(cpos) == 0:
+                            break
+                        else:
+                            found = True
+                            break
+                else:
+                    break
+            if found:
+                for xpos, ypos in cpos:
+                    self.board[xpos][ypos] = turn
+
+        if len(self._legal_actions(1 - self._cur_player)) == 0:
+            self._is_terminal = True
+            diff = np.sum(self.board)
+            if diff > 0:
+                self._player0_score = 1.0
+            elif diff < 0:
+                self._player0_score = -1.0
+            else:
+                self._player0_score = 0.0
+        else:
+            self._cur_player = 1 - self._cur_player
+
+    def _action_to_string(self, player, action):
+        """Action -> string."""
+        x, y = self._get_coord(action)
+        return f"({x}, {y})"
+
+    def is_terminal(self):
+        """Returns True if the game is over."""
+        return self._is_terminal
+
+    def returns(self):
+        """Total reward for each player over the course of the game so far."""
+        return [self._player0_score, -self._player0_score]
+
+    def __str__(self):
+        """String for debug purposes. No particular semantics are required."""
+        return _board_to_string(self.board)
+
+
+class BoardObserver:
+    """Observer, conforming to the PyObserver interface (see observation.py)."""
+
+    def __init__(self, params):
+        """Initializes an empty observation tensor."""
+        self.n = params.get("n", DEFAULT_N)
+        # The observation should contain a 1-D tensor in `self.tensor` and a
+        # dictionary of views onto the tensor, which may be of any shape.
+        # Here the observation is indexed `(cell state, row, column)`.
+        shape = (num_players, self.n, self.n)
+        self.tensor = np.zeros(np.prod(shape), np.float32)
+        self.dict = {"observation": np.reshape(self.tensor, shape)}
+
+    def set_from(self, state, player):
+        """Updates `tensor` and `dict` to reflect `state` from PoV of `player`."""
+        del player
+        # We update the observation via the shaped tensor since indexing is more
+        # convenient than with the 1-D tensor. Both are views onto the same memory.
+        obs = self.dict["observation"]
+        obs[0, :, :] = state.board == 1
+        obs[1, :, :] = state.board == -1
+
+    def string_from(self, state, player):
+        """Observation of `state` from the PoV of `player`, as a string."""
+        del player
+        return _board_to_string(state.board)
+
+
+def _board_to_string(board):
+    """Returns a string representation of the board."""
+    return "\n".join(" ".join(row) for row in board)
+
+
+# Register the game with the OpenSpiel library
+
+pyspiel.register_game(game_type, OthelloGame)
--- play.py
+++ play.py
@@ -4,21 +4,24 @@ import pygame
 from pygame import gfxdraw
 import pyspiel
 from open_spiel.python.algorithms import mcts
+from othello_game import OthelloGame, OthelloState
 
 pygame.init()
 pygame.font.init()
 
-myfont = pygame.font.SysFont('Comis Sans MS', 80)
-game_end_font = pygame.font.SysFont('Comis Sans MS', 200)
+myfont = pygame.font.SysFont("Comis Sans MS", 80)
+
+DEFAULT_N = 4
 
 pix = 80
 half = pix // 2
 linew = 1
-winw = (pix + linew) * 8 - linew
+winw = (pix + linew) * DEFAULT_N - linew
 background = (0, 144, 103)
 black = (0, 0, 0)
 grey = (48, 48, 48)
 white = (255, 255, 255)
+red = (255, 0, 0)
 
 display_width = winw
 display_height = winw
@@ -27,33 +30,45 @@ win = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Reversi")
 
 
-def redrawGameWindow(board, mboard):
+def draw_game_board(board, mboard):
     win.fill(background)
 
-    for x in range(1, 8):
+    for x in range(1, DEFAULT_N):
         pygame.draw.rect(win, black, (x * (pix + linew), 0, linew, winw))
-    for y in range(1, 8):
+    for y in range(1, DEFAULT_N):
         pygame.draw.rect(win, black, (0, y * (pix + linew), winw, linew))
 
-    for x in range(8):
-        for y in range(8):
+    for x in range(DEFAULT_N):
+        for y in range(DEFAULT_N):
             if board[x][y] == 1:
-                gfxdraw.filled_circle(win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black)
-                gfxdraw.aacircle(win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black)
+                gfxdraw.filled_circle(
+                    win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black
+                )
+                gfxdraw.aacircle(
+                    win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black
+                )
             elif board[x][y] == -1:
-                gfxdraw.filled_circle(win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, white)
-                gfxdraw.aacircle(win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black)
+                gfxdraw.filled_circle(
+                    win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, white
+                )
+                gfxdraw.aacircle(
+                    win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, black
+                )
 
-    for x in range(8):
-        for y in range(8):
+    for x in range(DEFAULT_N):
+        for y in range(DEFAULT_N):
             if mboard[x][y]:
-                gfxdraw.aacircle(win, x * (pix + linew) + half, y * (pix + linew) + half, half - 6, grey)
-
-    pygame.display.update()
+                gfxdraw.aacircle(
+                    win,
+                    x * (pix + linew) + half,
+                    y * (pix + linew) + half,
+                    half - 6,
+                    grey,
+                )
 
 
 if __name__ == "__main__":
-    game = pyspiel.load_game("othello")
+    game = pyspiel.load_game("othello_nxn")
     state = game.new_initial_state()
 
     # player1 is black, player2 is white
@@ -62,10 +77,13 @@ if __name__ == "__main__":
     # mcts ai
     rng = np.random.RandomState(42)
     evaluator = mcts.RandomRolloutEvaluator(n_rollouts=1, random_state=rng)
-    player2 = mcts.MCTSBot(game, uct_c=2, max_simulations=1000, evaluator=evaluator, random_state=rng)
+    player2 = mcts.MCTSBot(
+        game, uct_c=2, max_simulations=1000, evaluator=evaluator, random_state=rng
+    )
 
     clock = pygame.time.Clock()
     run = True
+    printed_winner = False
 
     while run:
         for event in pygame.event.get():
@@ -80,7 +98,7 @@ if __name__ == "__main__":
                         mx, my = pygame.mouse.get_pos()
                         mx = mx // (pix + linew)
                         my = my // (pix + linew)
-                        action = my + mx * 8
+                        action = my + mx * DEFAULT_N
                         if action in state.legal_actions():
                             state.apply_action(action)
                 else:
@@ -93,18 +111,42 @@ if __name__ == "__main__":
                         mx, my = pygame.mouse.get_pos()
                         mx = mx // (pix + linew)
                         my = my // (pix + linew)
-                        action = my + mx * 8
+                        action = my + mx * DEFAULT_N
                         if action in state.legal_actions():
                             state.apply_action(action)
                 else:
                     action = player2.step(state)
                     state.apply_action(action)
 
+        if state.is_terminal() and not printed_winner:
+            printed_winner = True
+            print("Winner: ", state.returns()[0])
+
+        board = (
+            np.array(state.observation_tensor(0)).reshape(2, DEFAULT_N, DEFAULT_N)[0]
+            - np.array(state.observation_tensor(0)).reshape(2, DEFAULT_N, DEFAULT_N)[1]
+        )
+        mboard = np.zeros(DEFAULT_N * DEFAULT_N, dtype=np.int8)
+        mboard[state.legal_actions()] = 1
+        draw_game_board(board, mboard.reshape(DEFAULT_N, DEFAULT_N))
+
+        if state.is_terminal():
+            if state.returns()[0] == 1:
+                text = myfont.render("Black Wins!", True, red)
+            elif state.returns()[0] == -1:
+                text = myfont.render("White Wins!", True, red)
+            else:
+                text = myfont.render("Draw!", True, red)
+            win.blit(
+                text,
+                (
+                    display_width // 2 - text.get_width() // 2,
+                    display_height // 2 - text.get_height() // 2,
+                ),
+            )
+
+        pygame.display.update()
+
         clock.tick(20)
-        board = np.array(state.observation_tensor(0)).reshape(3, 8, 8)[1] - np.array(state.observation_tensor(0)).reshape(3, 8, 8)[2]
-        mboard = np.zeros(64, dtype=np.int8)
-        if not state.is_terminal():
-            mboard[state.legal_actions()] = 1
-        redrawGameWindow(board, mboard.reshape(8, 8))
 
     pygame.quit()
